4ef478449fd73c17d55fa83e63b5e2cb72ff0e4e
Fedor Kiryakov <f.kiryakov@tbank.ru>
2024-10-30 13:54:06 +0300

    Make port vars final

--- source/lab1/main.dart
+++ source/lab1/main.dart
@@ -2,13 +2,13 @@ import 'calculator.dart';
 import 'number.dart';
 
 void main() async {
-  var c8rPort = await C8rSettings().spawn();
-  var numberAdd3Port = await NumberSettings(
+  final c8rPort = await C8rSettings().spawn();
+  final numberAdd3Port = await NumberSettings(
     value: 3,
     operation: (a, b) => a + b,
     calculatorPort: c8rPort,
   ).spawn();
-  var numberTimes2Port = await NumberSettings(
+  final numberTimes2Port = await NumberSettings(
     value: 2,
     operation: (a, b) => a * b,
     calculatorPort: c8rPort,
